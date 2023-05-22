f5ce6b72a407a29b312da7586eb58abf0fba9142
ken <kimkhay8@gmail.com>
2023-05-22 14:57:26 +0800

    lol

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 const port = 80
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api', (req, res) => {
-    res.send(req.body)
+app.post('/api', (req, res, next) => {
+    console.log(req.body)
+    res.send({status: true})
   })
 
 app.listen(port, () => {
